c930b2f1c90de320d69850a1640609bce6903b28
Sky Verma <52072723+Sky-Verma@users.noreply.github.com>
2020-09-11 09:41:39 +0530

    Updated syle.css and app.js

--- app.js
+++ app.js
@@ -1,71 +1,61 @@
-//This js file is loaded after the html one
-
-document.addEventListener("DOMContentLoaded", () => {
-  //Enter button will add task to the list
-    document.addEventListener('keypress', (e) => {
+document.addEventListener("DOMContentLoaded", function () {
+    // setting function for enter button
+    document.addEventListener("keypress", function (e) {
         if (e.keyCode === 13) {
             document.querySelector(".input-btn").click();
         }
-    });
-    
-    //Task done function
-    const checkItem = (e) => {
+    })
+    // function for deleting list items
+    function deleteItem(e) {
         e.preventDefault();
-        if (e.target.localName == "svg") {
-            let parentBtn = e.target.parentElement;
-            parentBtn.parentElement.style.textDecoration = "line-through";
-console.log(parentBtn.parentElement.innerHTML)
-        } else {
-            let parentBtn = e.target.parentElement.style.textDecoration = "line-through";
-            console.log(parentBtn.innerHTML)
-        }
-    }
-    
-    //Task delete function
-    const deleteItem = (e) => {
-
-        e.preventDefault();
-
         if (e.target.localName == 'svg') {
-
             let parentBtn = e.target.parentElement;
             parentBtn.parentElement.remove();
-        }
-
-        if (e.target.localName == 'button') {
-
+        } if (e.target.localName == 'button') {
             e.target.parentElement.remove();
         }
-
-
     }
-   //Add task function
-    const addItem = () => {
+    //function for checking the list item
+    function done(e) {
+        e.preventDefault();
+        if (e.target.localName == 'svg') {
+            let parentBtn = e.target.parentElement;
+            if (parentBtn.parentElement.style.textDecoration == 'line-through') {
+                parentBtn.parentElement.style.textDecoration = 'none'
+            } else {
+                parentBtn.parentElement.style.textDecoration = 'line-through';
+            }
 
+        } else {
+            if (e.target.parentElement.style.textDecoration == 'line-through') {
+                e.target.parentElement.styletextDecoration = 'none';
+            } else {
+                e.target.parentElement.styletextDecoration = 'line-through';
+            }
+        }
+    }
+    //Add function to add list item inside the unordered list
+    function addItem() {
         var input = document.querySelector(".todo-input");
-
-        if (input.value == "") {
-            alert("Enter some task.");
-            console.log("It worked.");
+        if (input.value === "") {
+            alert("Enter some task.")
         } else {
             var li = document.createElement('li');
             var del = document.createElement('button');
             var check = document.createElement('button');
-            check.innerHTML = '<i class="fas fa-check"></i>';
-            check.id='fa-check';
-            del.innerHTML = '<i class="fas fa-trash"></i>';
-            del.id = 'fa-trash';
+            check.innerHTML = ' <i class="fas fa-check"></i>';
+            check.id = "fa-check";
+            del.innerHTML = '<i class="fas fa-trash"> </i>';
+            del.id = "fa-trash";
             li.innerHTML = input.value;
             li.append(del);
             li.append(check);
             document.getElementById('main-list').append(li);
-            //Adding event listeners
+//adding eventlistener for deleteItem and done 
             del.addEventListener('click', deleteItem);
-            check.addEventListener("click", checkItem);
-            //Clearing textbox
-            input.value = "";
+            check.addEventListener('click', done);
+             input.value = "";
         }
     }
-    //Event listener for adding task
-    document.querySelector(".input-btn").addEventListener("click", addItem);
+document.querySelector(".input-btn").addEventListener("click", addItem)
 })
